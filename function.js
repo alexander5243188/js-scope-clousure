98aff424cd3d056f6f378285156448f125d076f1
alexander <alexander5243188@yahoo.es>
2023-02-16 16:55:27 -0400

    funtion scope

---
+++ function.js
@@ -0,0 +1,10 @@
+function greeting(){
+    let userName = 'graciela';
+    console.log(userName);
+
+    if(userName === 'graciale'){
+        console.log(`Hello ${userName}!`)
+    }
+}
+greeting();
+console.log(userName);
